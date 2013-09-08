ceb810284ed7d8e412e28a20a619a068e9db1c59
Gary Kacmarcik <garykac@google.com>
2013-09-08 10:27:38 -0700

    Organize the key value tables and add missing multimedia keys.
    Fix 21143 (add multimedia keys)
    Add multimedia keyboard section.
    Don't generate duplicate ids in tables (add support for 'noid' attribute to <key>s).
    Make table names consistent.

--- html/key-table-builder.js
+++ html/key-table-builder.js
@@ -1,13 +1,12 @@
 /* Convert <key> tags into a proper <table> of key info.
  * The document must be structured as follows:
  *   <div class="key-table">
- *     <key name="name" cat="cat" char="0000">Key description.</key>
+ *     <key name="name">Key description.</key>
  *     ...
  *   </div>
  * where:
  *   name: The name of the 'key' attribute.
- *   cat: The category (e.g.: 'General' or 'Math') of the key.
- *   char: (optional) The 4-digit hexadecimal Unicode value.
+ *   noid: (optional) Don't generate an id for this key.
  *
  * Multiple key-tables can be present in a single document.
  */
@@ -41,6 +40,8 @@ function create_key_table(tablediv) {
 			continue;
 		}
 		var keyname = key.getAttribute('name');
+		var noid = key.getAttribute('noid');
+		console.log(noid);
 		
 		row = table.insertRow(-1);
 		var code;
@@ -48,7 +49,8 @@ function create_key_table(tablediv) {
 		cell = row.insertCell(-1);
 		cell.setAttribute('class', 'key-table-key');
 		code = document.createElement('code');
-		code.id = 'key-' + keyname;
+		if (noid == null)
+			code.id = 'key-' + keyname;
 		code.setAttribute('class', 'key');
 		code.appendChild(document.createTextNode("'" + keyname + "'"));
 		cell.appendChild(code);
